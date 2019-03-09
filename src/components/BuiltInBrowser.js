38e015afca6c74f44ceba3a17f7d6b96e61e1be2
Ricardo Dantas <ricardodantas@gmail.com>
2019-03-09 17:13:26 +0100

    disabled browser

--- src/components/BuiltInBrowser.js
+++ src/components/BuiltInBrowser.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import { StyleSheet } from "react-native";
-import { WebView } from "react-native-webview";
+// import React, { Component } from "react";
+// import { StyleSheet } from "react-native";
+// import { WebView } from "react-native-webview";
 
-export default class BuiltInBrowser extends Component {
-  render() {
-    return (
-      <WebView
-        originWhitelist={["*"]}
-        source={{ uri: this.props.url }}
-        style={styles.browser}
-      />
-    );
-  }
-}
+// export default class BuiltInBrowser extends Component {
+//   render() {
+//     return (
+//       <WebView
+//         originWhitelist={["*"]}
+//         source={{ uri: this.props.url }}
+//         style={styles.browser}
+//       />
+//     );
+//   }
+// }
 
-const styles = StyleSheet.create({
-  browser: {
-    marginTop: 20
-  }
-});
+// const styles = StyleSheet.create({
+//   browser: {
+//     marginTop: 20
+//   }
+// });
